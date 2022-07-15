f7e9f25064b908f22e8721470a84cf20c414ab2e
Roos Cocolien <r.steigenga@gmail.com>
2022-07-15 14:33:15 +0200

    refactor: select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,69 +1,76 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers'
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
-	const displayedValue = getDisplayedValue(value, children)
+  const displayedValue = getDisplayedValue(value, children);
 
-	return (
-		<>
-			<Wrapper>
-				<NativeSelect id={label} value={value} onChange={onChange}>
-					{children}
-				</NativeSelect>
-				<Presentation>
-					{displayedValue}
-					<ChevronDownIcon id='chevron-down' strokeWidth={2} size={24} />
-				</Presentation>
-			</Wrapper>
-		</>
-	)
-}
+  return (
+    <>
+      <Wrapper>
+        <NativeSelect id={label} value={value} onChange={onChange}>
+          {children}
+        </NativeSelect>
+        <Presentation>
+          {displayedValue}
+          <ChevronDownIcon id="chevron-down" strokeWidth={2} size={24} />
+        </Presentation>
+      </Wrapper>
+    </>
+  );
+};
 
+/**
+ * The select element will have appearance none but it will sit
+ * front of the Wrapper which contains the Presentational div with the styling
+ * The wrapper should have position relative and this select should have
+ * position absolute. With its width and height set to 100%.
+ * This element will grow the size of its container.
+ **/
 const NativeSelect = styled.select`
-	appearance: none;
-	opacity: 0;
-	border: none;
-	position: absolute;
-	cursor: pointer;
-	width: 100%;
-	height: 100%;
-	top: 0;
-	left: 0;
-`
+  appearance: none; // or opacity zero?
+  opacity: 0;
+  border: none;
+  position: absolute;
+  cursor: pointer;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+`;
 
 const Presentation = styled.div`
-	top: 0;
-	left: 0;
-	font-size: 16px;
-	height: 100%;
-	background-color: ${COLORS.transparentGray15};
-	display: inline-flex;
-	justify-content: space-between;
-	align-items: center;
-	padding: 12px 18px 12px 16px;
-	border-radius: 8px;
-	color: ${COLORS.gray700};
+  top: 0;
+  left: 0;
+  font-size: 16px;
+  height: 100%;
+  background-color: ${COLORS.transparentGray15};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 12px 18px 12px 16px;
+  border-radius: 8px;
+  color: ${COLORS.gray700};
 
-	${NativeSelect}:focus + & {
-		outline: 2px solid ${COLORS.primary};
-	}
+  ${NativeSelect}:focus + & {
+    outline: 2px solid ${COLORS.primary};
+  }
 
-	${NativeSelect}:hover + & {
-		color: ${COLORS.black};
-	}
-`
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
 
-const Wrapper = styled.p`
-	position: relative;
-	display: inline-flex;
-`
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content; // this insures that also the select has a max-width
+`;
 
 const ChevronDownIcon = styled(Icon)`
-	margin-left: 24px;
-`
+  margin-left: 24px;
+`;
 
 export default Select;
